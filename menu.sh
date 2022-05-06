6aef34d212b0f6c64c1caf42b5961f81e5c41c56
Juan Diego Moreno <jdiiego95@gmail.com>
2022-05-06 13:35:42 -0500

    update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,49 +1,49 @@
 #!/bin/bash
 
 function  functions {
-    sudo sevice $sevice $1
+    sudo service $service $1
 }
 
-echo "Menu"
+echo "Menú"
 
-Opc1="Visualiza todos los sevicios"
-Opc2="Activa sevicio"
-Opc3="desactiva sevicio"
-Opc4="Reinicia sevicio"
-Opc5="Veifica estado de un sevicio"
-Opc6="Sali"
+Opc1="Visualizar todos los servicios"
+Opc2="Activar servicio"
+Opc3="desactiva servicio"
+Opc4="Reinicia servicio"
+Opc5="Verificar estado de un servicio"
+Opc6="Salir"
  
 select opc in "$Opc1" "$Opc2" "$Opc3" "$Opc4" "$Opc5" "$Opc6" ;
 do
     if [ "$opc" = "$Opc6"  ]; then 
-        echo "Hasta la póxima!" # Si quiee boar el archivo lo borra , si no salimos del programa mediante exit.
+        echo "Hasta la próxima!" 
         exit
     
     elif [ "$opc" = "$Opc5" ]; then 
-    echo "Esciba el nombe del sevicio veificar el estado"
-    ead sevice
+    echo "Escriba el nombre del servicio para verificar el estado"
+    read service
     functions status
 
     elif [ "$opc" = "$Opc4" ]; then 
-    echo "Esciba el nombe del sevicio que desea desactiva"
-    ead sevice
-    functions estat
+    echo "Escriba el nombre del servicio que desea reiniciar"
+    read service
+    functions restart
  
  
     elif [ "$opc" = "$Opc3" ]; then
-    echo "Esciba el nombe del sevicio que desea desactiva"
-    ead sevice
+    echo "Escriba el nombre del servicio que desea desactivar"
+    read service
     functions stop
  
  
     elif [ "$opc" = "$Opc2" ]; then
-    echo "Esciba el nombe del sevicio que desea activa"
-    ead sevice
-    functions stat
+    echo "Escriba el nombre del servicio que desea activar"
+    read service
+    functions start
  
  
     elif [ "$opc" = "$Opc1" ]; then # La opción1 nos pemite visualiza los pemisos mediante ls -la
-    echo "Listado de sevicios"
+    echo "Listado de servicios"
     functions --status-all
  
  
