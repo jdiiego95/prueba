5328de53d8339d391293312ce28aeda2e7c4d651
Juan Diego Moreno <jdiiego95@gmail.com>
2022-05-05 22:57:12 -0500

    update readme

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-# Introduction
-
-Lo que se pretende con este proyecto es crear un codigo que sirva para subir, bajar y reiniciar los servicios de linux
-
 # Historia de usuario
 
 **Subir y bajar servicios de linux**
@@ -16,3 +12,24 @@ Lo que se pretende con este proyecto es crear un codigo que sirva para subir, ba
 - Se deben generar 3 ramas, una de desarrollo, otra de pruebas y de producción, la cual almacenara la versión más sencilla
 - La rama de desarrollo generará un mensaje a un webhook cuando se active, desactive y restablezca un servicio
 - La rama de pruebas debe generar un mensaje y almacenarlo en un log cuando se active, desactive y restablezca un servicio
+
+# Introduction
+
+Lo que se pretende con este proyecto es crear un codigo que sirva para subir, bajar y reiniciar los servicios de linux
+
+## Instalación
+
+Para ejecutar el codigo, se debe ingresar a la terminal de lunix sobre la carpeta que se almaseno ejecutar `./menu.sh`
+
+## Uso
+
+### Rama producción
+
+Después de ejecutar el código sé mostrar un menú como el siguiente:
+![Image-1](https://i.postimg.cc/pXbH1dqW/Captura.jpg)
+
+En el cual, en la opción 1 le permitirá listar todos los servicios, ya sea los que estén activados o desactivados, en las demás opciones como se evidencia en los títulos el 2 permite Activar un servicio, el 3 desactivarlo, el 4 reiniciarlo y el 5 verificar en que estado se encuentra el servicio, y verificar que se esté ejecutando correctamente, en el uso solo se debe colocar el nombre del servicio y el algoritmo sé encargar de la función que escoja.
+
+### Rama Pruebas
+
+Tiene la misma ejecuacion que la rama anterior, solo que para esta rama se genero una carpeta llamada
--- menu.sh
+++ menu.sh
@@ -1,48 +1,49 @@
-#!/usr/bin/bash
+#!/bin/bash
+
 function  functions {
-    sudo service $service $1
+    sudo sevice $sevice $1
 }
 
 echo "Menu"
 
-Opc1="Visualizar todos los servicios"
-Opc2="Activar servicio"
-Opc3="desactivar servicio"
-Opc4="Reiniciar servicio"
-Opc5="Verificar estado de un servicio"
-Opc6="Salir"
+Opc1="Visualiza todos los sevicios"
+Opc2="Activa sevicio"
+Opc3="desactiva sevicio"
+Opc4="Reinicia sevicio"
+Opc5="Veifica estado de un sevicio"
+Opc6="Sali"
  
 select opc in "$Opc1" "$Opc2" "$Opc3" "$Opc4" "$Opc5" "$Opc6" ;
 do
     if [ "$opc" = "$Opc6"  ]; then 
-        echo "Hasta la próxima!" # Si quiere borrar el archivo lo borra , si no salimos del programa mediante exit.
+        echo "Hasta la póxima!" # Si quiee boar el archivo lo borra , si no salimos del programa mediante exit.
         exit
     
     elif [ "$opc" = "$Opc5" ]; then 
-    echo "Escriba el nombre del servicio verificar el estado"
-    read service
+    echo "Esciba el nombe del sevicio veificar el estado"
+    ead sevice
     functions status
 
     elif [ "$opc" = "$Opc4" ]; then 
-    echo "Escriba el nombre del servicio que desea desactivar"
-    read service
-    functions restart
+    echo "Esciba el nombe del sevicio que desea desactiva"
+    ead sevice
+    functions estat
  
  
     elif [ "$opc" = "$Opc3" ]; then
-    echo "Escriba el nombre del servicio que desea desactivar"
-    read service
+    echo "Esciba el nombe del sevicio que desea desactiva"
+    ead sevice
     functions stop
  
  
     elif [ "$opc" = "$Opc2" ]; then
-    echo "Escriba el nombre del servicio que desea activar"
-    read service
-    functions start
+    echo "Esciba el nombe del sevicio que desea activa"
+    ead sevice
+    functions stat
  
  
-    elif [ "$opc" = "$Opc1" ]; then # La opción1 nos permite visualizar los permisos mediante ls -la
-    echo "Listado de servicios"
+    elif [ "$opc" = "$Opc1" ]; then # La opción1 nos pemite visualiza los pemisos mediante ls -la
+    echo "Listado de sevicios"
     functions --status-all
  
  
